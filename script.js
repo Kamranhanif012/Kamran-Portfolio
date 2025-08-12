7cc80b8cf631a30a171e597dd8d97dc1e7d87263
Kamranhanif012 <mkamranhanif012@gmail.com>
2025-08-12 21:05:33 +0500

    skills added

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ toggleBtn.addEventListener('click' , ()=>{
       console.log('kamran')
       rotated ?  toggleLines.style.transform= 'rotate(0deg)': toggleLines.style.transform = 'rotate(90deg)'
       rotated ? dropLinks.style.display='none': dropLinks.style.display='block'
+      dropLinks.style.transition= 'transform 0.8s ease'; 
+      rotated ? dropLinks.style.transform= 'scaleY(0)' : dropLinks.style.transform= 'scaleY(1)'
+      
       
       rotated=!rotated
       

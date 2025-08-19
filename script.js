f0c664f37e895b4009be2bcb9cb21db0386d2be8
Kamranhanif012 <mkamranhanif012@gmail.com>
2025-08-19 19:51:09 +0500

    added: contact me and projects section

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-const toggleBtn= document.getElementById('drop-btn')
-const toggleLines= document.getElementById('rotating')
-const dropLinks= document.getElementById('droped-links')
+const toggleBtn = document.getElementById("drop-btn");
+const toggleLines = document.getElementById("rotating");
+const dropLinks = document.getElementById("droped-links");
 
 let rotated = false;
 
-toggleBtn.addEventListener('click' , ()=>{
-      console.log('kamran')
-      rotated ?  toggleLines.style.transform= 'rotate(0deg)': toggleLines.style.transform = 'rotate(90deg)'
-      rotated ? dropLinks.style.display='none': dropLinks.style.display='block'
-      dropLinks.style.transition= 'transform 0.8s ease'; 
-      rotated ? dropLinks.style.transform= 'scaleY(0)' : dropLinks.style.transform= 'scaleY(1)'
-      
-      
-      rotated=!rotated
-      
-})
+toggleBtn.addEventListener("click", () => {
+  console.log("kamran");
+  rotated
+    ? (toggleLines.style.transform = "rotate(0deg)")
+    : (toggleLines.style.transform = "rotate(90deg)");
+  rotated
+    ? (dropLinks.style.display = "none")
+    : (dropLinks.style.display = "block");
+  dropLinks.style.transition = "transform 0.8s ease";
+  rotated
+    ? (dropLinks.style.transform = "scaleY(0)")
+    : (dropLinks.style.transform = "scaleY(1)");
+
+  rotated = !rotated;
+});
